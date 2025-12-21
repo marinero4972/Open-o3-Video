c3d7042df43277c8c6832c8c15330e1e87cae689
postgnostic <firatyusuf.d@gmail.com>
2025-12-21 15:36:56 +0100

    change vllm version

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pip install -e ".[dev]"
 pip install "transformers==4.57.3"
 pip install wandb==0.18.3 tensorboardx qwen_vl_utils torchvision nltk rouge_score deepspeed wheel decord openpyxl xlsxwriter pysubs2 timm
 pip install flash_attn==2.7.4.post1 --no-build-isolation --no-cache-dir
-pip install vllm==0.7.2
+pip install vllm==0.11.0
